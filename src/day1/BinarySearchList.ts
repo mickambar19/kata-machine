c5b60622dc118aaebd235cb0ac7c9e0857cd77a5
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2025-06-16 20:22:18 -0600

    chore: implement binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,19 +1,19 @@
-// export default function bs_list(haystack: number[], needle: number): boolean {
-//     let low = 0;
-//     let high = haystack.length;
-//     while (low < high) {
-//         const middle = Math.floor((low + high) / 2);
-//         if (haystack[middle] === needle) {
-//             return true;
-//         }
-//         if (needle > haystack[middle]) {
-//             low = middle + 1;
-//         } else {
-//             high = middle;
-//         }
-//     }
-//     return false;
-// }
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+    while (low < high) {
+        const middle = Math.floor((low + high) / 2);
+        if (haystack[middle] === needle) {
+            return true;
+        }
+        if (needle > haystack[middle]) {
+            low = middle + 1;
+        } else {
+            high = middle;
+        }
+    }
+    return false;
+}
 
 // function binarySearch(arr: number[], needle: number): boolean {
 //     if (arr.length === 0) {
