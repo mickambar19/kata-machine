928a00048a229e93e4befa9728a68fc4c3efe505
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2025-06-16 20:39:54 -0600

    chore: implement Two Crystal Balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,1 +1,17 @@
-export default function two_crystal_balls(breaks: boolean[]): number {}
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpSize = Math.floor(Math.sqrt(breaks.length));
+    let firstBrokenIdx = 0;
+    for (let i = 0; i < breaks.length; i += jumpSize) {
+        if (breaks[i]) {
+            firstBrokenIdx = i;
+            break;
+        }
+    }
+
+    for (let i = firstBrokenIdx - jumpSize; i < firstBrokenIdx; i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
