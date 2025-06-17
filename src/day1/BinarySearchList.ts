89f0a473a52b5d1b0be3284d8ca6b32116a67361
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2025-06-16 20:19:59 -0600

    chore: bs

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,1 +1,35 @@
-export default function bs_list(haystack: number[], needle: number): boolean {}
+// export default function bs_list(haystack: number[], needle: number): boolean {
+//     let low = 0;
+//     let high = haystack.length;
+//     while (low < high) {
+//         const middle = Math.floor((low + high) / 2);
+//         if (haystack[middle] === needle) {
+//             return true;
+//         }
+//         if (needle > haystack[middle]) {
+//             low = middle + 1;
+//         } else {
+//             high = middle;
+//         }
+//     }
+//     return false;
+// }
+
+// function binarySearch(arr: number[], needle: number): boolean {
+//     if (arr.length === 0) {
+//         return false;
+//     }
+//     const middle = Math.floor(arr.length / 2);
+//     if (arr[middle] === needle) {
+//         return true;
+//     }
+
+//     if (needle > arr[middle]) {
+//         return binarySearch(arr.slice(middle + 1), needle);
+//     }
+//     return binarySearch(arr.slice(0, middle - 1), needle);
+// }
+
+// export default function bs_list(haystack: number[], needle: number): boolean {
+//     return binarySearch([...haystack], needle);
+// }
