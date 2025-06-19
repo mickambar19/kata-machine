b621ed52f83a8367c6f8bfb7194b45189ccb087f
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2025-06-18 19:22:47 -0600

    feat: implement Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,9 +1,38 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
 export default class Stack<T> {
     public length: number;
+    private head?: Node<T>;
 
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this.head = undefined;
+    }
 
-    push(item: T): void {}
-    pop(): T | undefined {}
-    peek(): T | undefined {}
+    push(item: T): void {
+        const newNode: Node<T> = { value: item };
+        this.length++;
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+    pop(): T | undefined {
+        if (!this.head) {
+            return;
+        }
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+        head.next = undefined;
+        return head.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
