9b4531668393e4da4ee289bf7de81cc691924e14
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2025-06-18 19:22:03 -0600

    chore: implement Queue first attempt

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,9 +1,46 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
 export default class Queue<T> {
     public length: number;
+    private head?: Node<T>;
+    private tail?: Node<T>;
+    constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
+    }
 
-    constructor() {}
+    enqueue(item: T): void {
+        const newNode: Node<T> = { value: item };
+        this.length++;
+        if (!this.tail) {
+            this.head = this.tail = newNode;
+            return;
+        }
 
-    enqueue(item: T): void {}
-    deque(): T | undefined {}
-    peek(): T | undefined {}
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
+
+    deque(): T | undefined {
+        if (!this.tail) {
+            console.log("Empty");
+            return;
+        }
+        this.length--;
+        const curr = this.head;
+
+        if (this.length === 0) {
+            this.head = this.tail = undefined;
+        } else {
+            this.head = this.head?.next;
+        }
+
+        return curr?.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
